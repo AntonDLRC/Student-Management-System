333bc8f7f0304464227c70a700d55336b417d197
Anton <cantillanoanton0@gmail.com>
2025-04-14 15:22:49 +1200

    added signup code and comments about other functions

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, session, redirect
 import sqlite3
+#super cool functions to generate and check password password hashes
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 DATABASE = "database.db"
 
+#This query_db function combines getting the databse, cursor, executing and fetching the results
 def query_db(sql, args=(), one=False):
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
@@ -33,8 +36,21 @@ def student(id):
  JOIN StudentCourses ON StudentCourses.StudentID = Students.ID
  JOIN Courses ON Courses.ID = StudentCourses.CourseID 
  WHERE Students.ID = ?;"""
-    result = query_db(sql, (id,))
+    result = query_db(sql, (id,)) #this query_db is not (sql, (id,) True) because the courses 
+                                  # have multiple rows
     return render_template("student_details.html", student = result)
+
+# signup page
+@app.route("/signup", methods = ["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form("username")
+        password = request.form("password")
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+        sql = "INSERT INTO User (username, password) VALUES (?, ?);"
+        query_db(sql, (username, hashed_password))
+        # flash to show that the account was created
+        flash("Account created successfully!")
 
 
 if __name__ == "__main__":
